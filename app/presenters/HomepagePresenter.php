95e2b400bd58fdaa40e656da179ae823bde59713
David Cepelik <cepelik@gymlit.cz>
2016-01-27 21:07:27 +0100

    don't scream when there's no wmrq data available

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -11,7 +11,7 @@ class HomepagePresenter extends Nette\Application\UI\Presenter
 	public function renderDefault()
 	{
 		$output = shell_exec('wmrq 127.0.0.1 20892');
-		$yaml = yaml_parse($output, -1);
+		$yaml = @yaml_parse($output, -1) ?: [];
 
 		$readings = [];
 		foreach ($yaml as $reading) {
